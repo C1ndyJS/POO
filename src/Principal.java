e44e3301402950a0de0049b8634b101c66f54cb1
Cindy <cindy.jimenez@utp.edu.co>
2025-07-10 13:42:39 -0500

    Ejercicio Practico POO Java

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,20 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Star Wars";
-        miPelicula.fechaLanzamiento= 2000;
-        miPelicula.duracionMin = 120;
-        miPelicula.incluidoPlan= true;
+        miPelicula.setNombre("Star Wars");
+        miPelicula.setFechaLanzamiento(2000);
+        miPelicula.setDuracionMin(200) ;
+        miPelicula.setIncluidoPlan(true);
+
         miPelicula.mostrarFichaTecnica();
         miPelicula.evaluacion(10);
         miPelicula.evaluacion(8);
         System.out.println("Evaluacion: "+ miPelicula.calcMedia());
+        System.out.println("Evaluacion: "+ miPelicula.getTotalEvaluaciones());
 
-//        Pelicula otraPelicula = new Pelicula();
+//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
 //        otraPelicula.nombre = "Jumanji";
 //        otraPelicula.fechaLanzamiento= 2008;
 //        otraPelicula.incluidoPlan= false;
